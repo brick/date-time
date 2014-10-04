fc4b4a77cc333889ff1a150abbe69b1245905d8c
Benjamin Morel <benjamin.morel@gmail.com>
2014-10-04 14:04:41 +0200

    Added a Stopwatch to measure elapsed time

---
+++ src/Stopwatch.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Brick\DateTime;
+
+/**
+ * Measures the time elapsed.
+ */
+class Stopwatch
+{
+    /**
+     * The total time the stopwatch has been running, excluding the time elapsed since it was started.
+     *
+     * Every time the stopwatch is stopped, the elapsed time is added to this value.
+     *
+     * @var Duration
+     */
+    private $duration;
+
+    /**
+     * The time the stopwatch has been started at, or null if it is not running.
+     *
+     * @var Instant|null
+     */
+    private $startTime;
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+        $this->duration = Duration::zero();
+    }
+
+    /**
+     * Starts the timer.
+     *
+     * If the timer is already started, this method does nothing.
+     *
+     * @return void
+     */
+    public function start()
+    {
+        if (! $this->startTime) {
+            $this->startTime = Instant::now();
+        }
+    }
+
+    /**
+     * Stops the timer.
+     *
+     * If the timer is already stopped, this method does nothing.
+     *
+     * @return void
+     */
+    public function stop()
+    {
+        if ($this->startTime === null) {
+            return;
+        }
+
+        $this->duration = $this->duration->plus(Duration::between($this->startTime, Instant::now()));
+        $this->startTime = null;
+    }
+
+    /**
+     * Returns the time this stopwatch has been started at, or null if it is not running.
+     *
+     * @return Instant|null
+     */
+    public function getStartTime()
+    {
+        return $this->startTime;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isRunning()
+    {
+        return $this->startTime !== null;
+    }
+
+    /**
+     * Returns the total elapsed time.
+     *
+     * This includes the times between previous start() and stop() calls if any,
+     * as well as the time since the stopwatch was last started if it is running.
+     *
+     * @return Duration
+     */
+    public function getElapsedTime()
+    {
+        if ($this->startTime === null) {
+            return $this->duration;
+        }
+
+        return $this->duration->plus(Duration::between($this->startTime, Instant::now()));
+    }
+}
---
+++ tests/StopwatchTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Brick\DateTime\Tests;
+
+use Brick\DateTime\Stopwatch;
+
+/**
+ * Unit tests for class Period.
+ */
+class StopwatchTest extends AbstractTestCase
+{
+    /**
+     * @return Stopwatch
+     */
+    public function testNew()
+    {
+        $stopwatch = new Stopwatch();
+
+        $this->assertNull($stopwatch->getStartTime());
+        $this->assertFalse($stopwatch->isRunning());
+        $this->assertDurationIs(0, 0, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testNew
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testStart(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(1000, 1);
+
+        $stopwatch->start();
+
+        $this->assertReadableInstantIs(1000, 1, $stopwatch->getStartTime());
+        $this->assertTrue($stopwatch->isRunning());
+        $this->assertDurationIs(0, 0, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testStart
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testElapsedTimeWhileRunning(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(2000, 0);
+
+        $this->assertReadableInstantIs(1000, 1, $stopwatch->getStartTime());
+        $this->assertTrue($stopwatch->isRunning());
+        $this->assertDurationIs(999, 999999999, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testElapsedTimeWhileRunning
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testStop(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(3000, 2);
+
+        $stopwatch->stop();
+
+        $this->assertNull($stopwatch->getStartTime());
+        $this->assertFalse($stopwatch->isRunning());
+        $this->assertDurationIs(2000, 1, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testStop
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testFrozenAfterStop(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(4000, 9);
+
+        $this->assertNull($stopwatch->getStartTime());
+        $this->assertFalse($stopwatch->isRunning());
+        $this->assertDurationIs(2000, 1, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testFrozenAfterStop
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testRestart(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(5000, 9);
+
+        $stopwatch->start();
+
+        $this->assertReadableInstantIs(5000, 9, $stopwatch->getStartTime());
+        $this->assertTrue($stopwatch->isRunning());
+        $this->assertDurationIs(2000, 1, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testRestart
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testElapsedTimeWhileRunningAfterRestart(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(5001, 10);
+
+        $this->assertReadableInstantIs(5000, 9, $stopwatch->getStartTime());
+        $this->assertTrue($stopwatch->isRunning());
+        $this->assertDurationIs(2001, 2, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testElapsedTimeWhileRunningAfterRestart
+     *
+     * @param Stopwatch $stopwatch
+     *
+     * @return Stopwatch
+     */
+    public function testStopAgain(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(5002, 20);
+
+        $stopwatch->stop();
+
+        $this->assertNull($stopwatch->getStartTime());
+        $this->assertFalse($stopwatch->isRunning());
+        $this->assertDurationIs(2002, 12, $stopwatch->getElapsedTime());
+
+        return $stopwatch;
+    }
+
+    /**
+     * @depends testStopAgain
+     *
+     * @param Stopwatch $stopwatch
+     */
+    public function testFrozenAfterSecondStop(Stopwatch $stopwatch)
+    {
+        $this->setClockTime(6000, 999);
+
+        $this->assertNull($stopwatch->getStartTime());
+        $this->assertFalse($stopwatch->isRunning());
+        $this->assertDurationIs(2002, 12, $stopwatch->getElapsedTime());
+    }
+}
