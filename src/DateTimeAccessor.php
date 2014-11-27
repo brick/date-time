746529c041fefbcc7b5baf80df8d95cdbbeb1c27
Benjamin Morel <benjamin.morel@gmail.com>
2014-11-27 15:07:05 +0100

    Fixed DateTimeAccessor documentation
    getField() does not throw an exception but returns null when the field is not supported.

--- src/DateTimeAccessor.php
+++ src/DateTimeAccessor.php
@@ -13,8 +13,6 @@ interface DateTimeAccessor
      * @param string $field The date-time field.
      *
      * @return integer|null
-     *
-     * @throws DateTimeException If the field is not supported by this accessor.
      */
     public function getField($field);
 }
