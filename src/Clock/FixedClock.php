c08e7e73351ae8d97fd3e9f7855672e352049db4
Benjamin Morel <benjamin.morel@gmail.com>
2017-10-04 16:30:52 +0200

    Add FixedClock::move()

--- src/Clock/FixedClock.php
+++ src/Clock/FixedClock.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Brick\DateTime\Clock;
 
 use Brick\DateTime\Clock;
+use Brick\DateTime\Duration;
 use Brick\DateTime\Instant;
 
 /**
@@ -43,5 +44,19 @@ class FixedClock implements Clock
     public function setTime(Instant $instant) : void
     {
         $this->instant = $instant;
+    }
+
+    /**
+     * Moves the clock by a number of seconds and/or nanos.
+     *
+     * @param int $seconds
+     * @param int $nanos
+     *
+     * @return void
+     */
+    public function move(int $seconds, int $nanos = 0) : void
+    {
+        $duration = Duration::ofSeconds($seconds, $nanos);
+        $this->instant = $this->instant->plus($duration);
     }
 }
