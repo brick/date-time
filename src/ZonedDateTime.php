c0ba4220b01980a1fd818dacfda273191e30efcd
Benjamin Morel <benjamin.morel@gmail.com>
2021-01-16 23:04:22 +0100

    Remove now useless psalm-suppress

--- src/ZonedDateTime.php
+++ src/ZonedDateTime.php
@@ -232,9 +232,6 @@ class ZonedDateTime implements \JsonSerializable
 
         $dateTimeZone = $dateTime->getTimezone();
 
-        /**
-         * @psalm-suppress TypeDoesNotContainType - see https://github.com/vimeo/psalm/issues/4515
-         */
         if ($dateTimeZone === false) {
             throw new DateTimeException('This DateTime object has no timezone.');
         }
